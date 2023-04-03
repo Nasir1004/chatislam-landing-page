17625c0c5c32bfb9d6ed7d6eb38f76449b5eaee4
Nasir Lawal <nasirlawal001@gmail.com>
2023-04-03 17:10:29 +0100

    another commit of the day

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -2,11 +2,11 @@
   <div class="tw-mt-12 sm:tw-mt-24">
     <div>
       <div class="title tw-text-2xl sm:tw-text-3xl tw-text-center">
-        Meet Our Team
+        Scholars
       </div>
-      <div class="row tw-justify-center sm:tw-mt-8">
+      <div class="row tw-justify-center tw-mt-8">
         <div
-          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-mt-4 sm:tw-mt-0 tw-border-primary-bg-color tw-rounded-2xl"
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
         >
           <div class="row tw-justify-center">
             <q-img
@@ -18,11 +18,11 @@
             Nasir Ibrahim Abba
           </div>
           <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
-            Co-founder Eduvacity & founder ChatIslam
+            Co-Founder Eduvacity & Founder ChatIslam
           </div>
         </div>
         <div
-          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-mt-4 sm:tw-mt-0 tw-border-primary-bg-color tw-rounded-2xl"
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
         >
           <div class="row tw-justify-center">
             <q-img
@@ -31,14 +31,14 @@
             />
           </div>
           <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
-            Bashir Isah Dodo
+            Bashir Isah Dodo PhD
           </div>
           <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
-            PhD, Ai resercher & Co-founder ChatIslam
+            Co-Founder ChatIslam & Ai Resercher
           </div>
         </div>
         <div
-          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-mt-4 sm:tw-mt-0 tw-border-primary-bg-color tw-rounded-2xl"
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
         >
           <div class="row tw-justify-center">
             <q-img
@@ -50,11 +50,11 @@
             Hajar Mousannif
           </div>
           <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
-            Associate Professor | AI, ML & TinyML Advocate |
+            Associate Professor | AI, ML & TinyML Advocate
           </div>
         </div>
         <div
-          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-mt-4 sm:tw-mt-0 tw-border-primary-bg-color tw-rounded-2xl"
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
         >
           <div class="row tw-justify-center">
             <q-img
@@ -63,21 +63,72 @@
             />
           </div>
           <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
-            Najeeb G. Abdulhamid
+            Najeeb G. Abdulhamid PhD
           </div>
           <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
             Resercher @Microsoft
           </div>
         </div>
       </div>
-      <div class="row tw-justify-center tw-mt-12">
-        <q-btn
-          outline
-          no-caps
-          class="tw-w-36 tw-rounded-full"
-          label="Join Our Team"
-          @click="onClick"
-        />
+      <div class="row tw-justify-center sm:tw-mt-8">
+        <div
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/yusuf.jpg"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Yusuf Abubakar PhD
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Ai Resercher Ahmadu Bello University
+          </div>
+        </div>
+        <div
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/mustapha.jpg"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Mustapha Jimoh
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Ai Resercher & machine learning engineer
+          </div>
+        </div>
+        <div
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/nasir.png"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Dahir Muhammad Dahir
+          </div>
+        </div>
+        <div
+          class="tw-w-64 tw-h-56 card tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl tw-mt-4 sm:tw-mt-0"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/nasir.png"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Nasir Ibrahim Abba
+          </div>
+        </div>
       </div>
     </div>
   </div>
